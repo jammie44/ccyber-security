ce94ec21edff8861c410b2969d10832e8f4479ec
jammie44 <jammietristan@gmail.com>
2026-07-14 12:24:46 +0300

    Delete risk (17).py

--- risk (17).py
+++
@@ -1,5 +0,0 @@
-from app.repositories.base import BaseRepository
-from app.repositories.asset_repository import AssetRepository
-from app.repositories.vulnerability_repository import VulnerabilityRepository
-
-__all__ = ["BaseRepository", "AssetRepository", "VulnerabilityRepository"]
